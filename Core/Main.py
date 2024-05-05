83ad2208c83f4c51090e35acb8a690eb83208020
Samsa <vmchicha@gmail.com>
2024-05-05 23:31:26 +0300

    fix errors

--- Core/Main.py
+++ Core/Main.py
@@ -208,7 +208,7 @@ def main(page: Page):
 
 def Start(web=True):
     if web:
-        host, port = '192.168.0.109', 9876
+        host, port = '127.0.0.1', 9876
         banner(host, port)
         app(main, view='web_browser', host=host, port=port)
     else:
